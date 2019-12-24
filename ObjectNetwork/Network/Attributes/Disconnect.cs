255ec739c4ab95f233d9a0e15d0011951dcd1c77
Jonas Klemmensen <jonastklemmensen@gmail.com>
2019-12-24 17:02:24 +0100

    Added Disconnect attribute

---
+++ ObjectNetwork/Network/Attributes/Disconnect.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectNetwork.Network.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class Disconnect : System.Attribute
+    {
+        public Disconnect()
+        {
+
+        }
+    }
+}
