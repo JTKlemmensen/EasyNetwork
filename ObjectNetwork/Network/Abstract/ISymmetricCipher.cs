af2db45094976d625f33a12931a23ed07b7ae570
Jonas Klemmensen <jonastklemmensen@gmail.com>
2019-12-24 16:44:08 +0100

    Added ISymmetricCipher

---
+++ ObjectNetwork/Network/Abstract/ISymmetricCipher.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectNetwork.Network.Abstract
+{
+    public interface ISymmetricCipher
+    {
+        /// <summary>
+        /// Gets or sets the Key part of symmetric algorithm
+        /// </summary>
+        byte[] Key { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Initialization Vector part of symmetric algorithm
+        /// </summary>
+        byte[] IV { get; set; }
+
+        /// <summary>
+        /// Encrypts data based on the Key and IV properties.
+        /// </summary>
+        byte[] Encrypt(byte[] data);
+
+        /// <summary>
+        /// Decrypts data based on the Key and IV properties.
+        /// </summary>
+        byte[] Decrypt(byte[] data);
+    }
+}
