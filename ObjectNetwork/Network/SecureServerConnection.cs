82911630772b97886bba2061fd7e49bd623d0051
Jonas Klemmensen <jonastklemmensen@gmail.com>
2019-12-25 00:58:08 +0100

    Implemented SecureServerConnection

---
+++ ObjectNetwork/Network/SecureServerConnection.cs
@@ -0,0 +1,40 @@
+﻿using ObjectNetwork.Network.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectNetwork.Network
+{
+    public class SecureServerConnection : SecureConnection
+    {
+        public override event Connected OnConnected;
+        private bool handshakeDone;
+
+        public SecureServerConnection(IConnection connection, IAsymmetricCipher asymmetricCipher, ISymmetricCipher symmetricCipher)
+            : base(connection, asymmetricCipher, symmetricCipher)
+        {
+
+        }
+
+        protected override void OnEncryptedMessageReceived(byte[] data)
+        {
+            if (handshakeDone)
+                base.OnEncryptedMessageReceived(data);
+            else
+                EndHandshake(data);
+        }
+
+        /// <summary>
+        /// Ends handshake by sending the symmetric iv and key to the remote peer
+        /// </summary>
+        private void EndHandshake(byte[] data)
+        {
+            AsymmetricCipher.PublicKey = data;
+            Connection.SendData(AsymmetricCipher.Encrypt(SymmetricCipher.IV));
+            Connection.SendData(AsymmetricCipher.Encrypt(SymmetricCipher.Key));
+            handshakeDone = true;
+
+            OnConnected?.Invoke();
+        }
+    }
+}
