94b52212230a39cf5065bb729d32aa87d0e12b3b
Jonas Klemmensen <jonastklemmensen@gmail.com>
2019-12-24 16:53:13 +0100

    Implemented AESSymmetricCipher

---
+++ ObjectNetwork/Network/AESSymmetricCipher.cs
@@ -0,0 +1,60 @@
+﻿using ObjectNetwork.Network.Abstract;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ObjectNetwork.Network
+{
+    public class AESSymmetricCipher : ISymmetricCipher
+    {
+        private Aes cipher;
+
+        public AESSymmetricCipher()
+        {
+            cipher = Aes.Create();
+            cipher.KeySize = 128;
+        }
+
+        public byte[] Key
+        {
+            get => cipher.Key;
+            set => cipher.Key = value;
+        }
+
+        public byte[] IV
+        {
+            get => cipher.IV;
+            set => cipher.IV = value;
+        }
+
+        public byte[] Decrypt(byte[] data)
+        {
+            return EncryptOrDecrypt(data, cipher.CreateDecryptor());
+        }
+
+        public byte[] Encrypt(byte[] data)
+        {
+            return EncryptOrDecrypt(data, cipher.CreateEncryptor());
+        }
+
+        private byte[] EncryptOrDecrypt(byte[] data, ICryptoTransform type)
+        {
+            try
+            {
+                using (MemoryStream memoryStream = new MemoryStream())
+                using (CryptoStream cryptoStream = new CryptoStream(memoryStream, type, CryptoStreamMode.Write))
+                {
+                    cryptoStream.Write(data, 0, data.Length);
+                    cryptoStream.FlushFinalBlock();
+                    return memoryStream.ToArray();
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
