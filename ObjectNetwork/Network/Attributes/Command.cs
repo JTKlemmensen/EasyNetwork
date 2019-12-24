7da52758af0bde17ed8f869408e7efcb41e81a16
Jonas Klemmensen <jonastklemmensen@gmail.com>
2019-12-24 17:01:14 +0100

    Added Command attribute

---
+++ ObjectNetwork/Network/Attributes/Command.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectNetwork.Network.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class Command : System.Attribute
+    {
+        public Command()
+        {
+
+        }
+    }
+}
