62be83feefda7d5dcbedc93cfcb45eb78977e283
Jonas Klemmensen <jonastklemmensen@gmail.com>
2019-12-24 17:04:29 +0100

    Added IDataFormatters

---
+++ ObjectNetwork/Network/Abstract/IDataFormatters.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectNetwork.Network.Abstract
+{
+    /// <summary>
+    /// Stores a ISerializer, ISymmetricCipher and a IAsymmetricCipher
+    /// </summary>
+    public interface IDataFormatters
+    {
+        ISerializer Serializer { get; }
+        ISymmetricCipher SymmetricCipher { get; }
+        IAsymmetricCipher AsymmetricCipher { get; }
+    }
+}
