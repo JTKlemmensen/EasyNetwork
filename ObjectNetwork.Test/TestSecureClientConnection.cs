c0a04b451d45813789439b2ae51fba4a93e4a195
Jonas Klemmensen <jonastklemmensen@gmail.com>
2019-12-25 01:47:22 +0100

    Implemented tests for SecureClientConnection

---
+++ ObjectNetwork.Test/TestSecureClientConnection.cs
@@ -0,0 +1,88 @@
+﻿using Moq;
+using NUnit.Framework;
+using ObjectNetwork.Network;
+using ObjectNetwork.Network.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectNetwork.Test
+{
+    public class TestSecureClientConnection
+    {
+        [Test]
+        public void Test_StartHandshake()
+        {
+            var publicKey = new byte[] { 1, 2, 3 };
+            var mockAsymmetric = new Mock<IAsymmetricCipher>();
+            mockAsymmetric.Setup(m => m.PublicKey).Returns(publicKey);
+            var mockSymmetric = new Mock<ISymmetricCipher>();
+            var mockConnection = new Mock<IConnection>();
+
+            var secureClient = new SecureClientConnection(mockConnection.Object, mockAsymmetric.Object, mockSymmetric.Object);
+            secureClient.Start();
+
+            mockConnection.Verify(m => m.SendData(publicKey));
+        }
+
+        [Test]
+        public void Test_EndHandshake()
+        {
+            var publicKey = new byte[] { 1, 2, 3 };
+            var iv = new byte[] { 1, 2 };
+            var key = new byte[] { 1, 2 };
+            var mockAsymmetric = new Mock<IAsymmetricCipher>();
+            mockAsymmetric.Setup(m => m.PublicKey).Returns(publicKey);
+            mockAsymmetric.Setup(m => m.Decrypt(iv)).Returns(iv);
+            mockAsymmetric.Setup(m => m.Decrypt(key)).Returns(key);
+
+            var mockSymmetric = new Mock<ISymmetricCipher>();
+            mockSymmetric.SetupSet(m => m.IV = It.IsAny<byte[]>()).Verifiable();
+            mockSymmetric.SetupSet(m => m.Key = It.IsAny<byte[]>()).Verifiable();
+
+            var mockConnection = new Mock<IConnection>();
+
+            var secureClient = new SecureClientConnection(mockConnection.Object, mockAsymmetric.Object, mockSymmetric.Object);
+            secureClient.Start();
+
+            bool OnConnectedHasBeenCalled = false;
+            secureClient.OnConnected += () => { OnConnectedHasBeenCalled = true; };
+            // Act
+            mockConnection.Raise(i => i.OnDataReceived += null, iv);
+            mockConnection.Raise(i => i.OnDataReceived += null, key);
+
+            mockAsymmetric.Verify(m => m.Decrypt(iv));
+            mockAsymmetric.Verify(m => m.Decrypt(key));
+            mockSymmetric.VerifySet(m => m.IV = iv);
+            mockSymmetric.VerifySet(m => m.Key = key);
+
+            Assert.IsTrue(OnConnectedHasBeenCalled);
+        }
+
+        [Test]
+        public void Test_ReceiveDataAfterHandshake()
+        {
+            var data = new byte[] { 2, 4, 6 };
+            var encryptedData = new byte[] { 4, 6, 8 };
+            var mockAsymmetric = new Mock<IAsymmetricCipher>();
+
+            var mockSymmetric = new Mock<ISymmetricCipher>();
+            mockSymmetric.Setup(m => m.Decrypt(encryptedData)).Returns(data).Verifiable();
+
+            var mockConnection = new Mock<IConnection>();
+
+            var secureClient = new SecureClientConnection(mockConnection.Object, mockAsymmetric.Object, mockSymmetric.Object);
+
+            mockConnection.Raise(i => i.OnDataReceived += null, new byte[] { });
+            mockConnection.Raise(i => i.OnDataReceived += null, new byte[] { });
+            byte[] receivedData = null;
+            secureClient.OnDataReceived += (data) => receivedData = data;
+
+            //Act
+            mockConnection.Raise(i => i.OnDataReceived += null, encryptedData);
+
+            mockSymmetric.Verify(m => m.Decrypt(encryptedData));
+            Assert.AreEqual(data, receivedData);
+        }
+    }
+}
