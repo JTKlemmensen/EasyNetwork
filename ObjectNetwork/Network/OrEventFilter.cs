9d61cf7fecd6e47e2d346071004ef8aa73cee77f
Jonas Klemmensen <jonastklemmensen@gmail.com>
2019-12-25 21:23:06 +0100

    Implemented AndEventFilter & OrEventFilter

---
+++ ObjectNetwork/Network/OrEventFilter.cs
@@ -0,0 +1,23 @@
+﻿using ObjectNetwork.Network.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace ObjectNetwork.Network
+{
+    /// <summary>
+    /// Combines 2 EventFilters in to 1. Returns true if either one return true or are null
+    /// and returns false if both return false
+    /// </summary>
+    public class OrEventFilter : IEventFilter
+    {
+        public IEventFilter Left { get; set; }
+        public IEventFilter Right { get; set; }
+        public Func<bool> GenerateFunc(MethodInfo methodInfo)
+        {
+            return () => (Left?.GenerateFunc(methodInfo).Invoke() ?? true) ||
+                         (Right?.GenerateFunc(methodInfo).Invoke() ?? true);
+        }
+    }
+}
