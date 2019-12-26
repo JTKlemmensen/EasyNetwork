c7f6f7152c2d4595eb1b9202fe515e6a87443d36
Jonas Klemmensen <jonastklemmensen@gmail.com>
2019-12-26 22:19:48 +0100

    Renamed root folder to EasyNetwork

---
+++ EasyNetwork.Test/TestBuilder.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+using ObjectNetwork.Network;
+using ObjectNetwork.Network.Abstract;
+using ObjectNetwork.Network.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ObjectNetwork.Test
+{
+    public class TestBuilder
+    {
+        [Test]
+        public void Test()
+        {
+            Task.Run(RunServer);
+            Thread.Sleep(500);
+            Task.Run(RunClient);
+
+            Thread.Sleep(25000);
+        }
+
+        private void RunClient()
+        {
+            new ConnectionBuilder()
+                .AddEventHandler(new TestBuilder())
+                .AddEventHandler(new IdleChecker())
+                .CreateClient("127.0.0.1", 25000).Start();
+        }
+
+        private void RunServer()
+        {
+            var listener = new ConnectionBuilder()
+                .AddEventHandler(new TestBuilder())
+                .AddEventHandler(new IdleChecker())
+                .CreateServer(25000);
+            listener.Start();
+        }
+
+        [Connect]
+        public void OnConnect(IObjectConnection connection)
+        {
+            Debug.WriteLine("Ip: " + connection.Ip);
+            Task.Run(Slow);
+            //connection.SendObject("Hello world!");
+        }
+
+        private async Task Slow()
+        {
+            await Task.Delay(500);
+            Debug.WriteLine("Connected0000000");
+        }
+
+        [Disconnect]
+        public void OnConnect2(DefaultObjectConnection connection)
+        {
+            Debug.WriteLine("DISCONNECTET"); 
+        }
+
+        [Command]
+        public void OnMessage(DefaultObjectConnection connection, PingObject message)
+        {
+            Debug.WriteLine("Received Message: ");
+        }
+    }
+}
