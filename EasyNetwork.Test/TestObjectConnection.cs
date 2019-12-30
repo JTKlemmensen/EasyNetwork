f1fb2fbdaea65d4a0eeefdd7dd740751f3df4a2d
Jonas Klemmensen <jonastklemmensen@gmail.com>
2019-12-30 23:44:49 +0100

    Implemented tests for Lambda event handlers (Connect, Command & Disconnect)

--- EasyNetwork.Test/TestObjectConnection.cs
+++ EasyNetwork.Test/TestObjectConnection.cs
@@ -295,6 +295,274 @@ namespace EasyNetwork.Test
             Assert.IsFalse(testHandler.HasOnCommandBeenCalled);
         }
 
+        [Test]
+        public void Test_Lambda_OnCommand()
+        {
+            var testHandler = new TestHandler();
+
+            byte[] serializedData = new byte[] { 1, 2, 3 };
+            byte[] serializedMessage = new byte[] { 1, 2, 3, 4 };
+            string unserializedData = "some data";
+            var mockSerializer = new Mock<ISerializer>();
+            mockSerializer.Setup(m => m.Deserialize<NetworkMessage>(serializedMessage)).Returns(new NetworkMessage { Data = serializedData, Name = "System.String" });
+            mockSerializer.Setup(m => m.Deserialize<string>(serializedData)).Returns(unserializedData);
+
+            var mockConnection = new Mock<IConnection>();
+            var mockEventManager = new Mock<IEventManager>();
+
+            var objectConnection = new DefaultObjectConnection(mockConnection.Object) { Manager = mockEventManager.Object, Serializer = mockSerializer.Object };
+            bool OnCommandHasBeenCalled = false;
+            objectConnection.OnCommand<string>((c, s) => OnCommandHasBeenCalled = true);
+
+            //Act
+            mockConnection.Raise(m => m.OnDataReceived += null, serializedMessage);
+
+            Assert.IsTrue(OnCommandHasBeenCalled);
+        }
+        
+        [Test]
+        public void Test_Lambda_OnConnect()
+        {
+            var testHandler = new TestHandler();
+
+            byte[] serializedData = new byte[] { 1, 2, 3 };
+            byte[] serializedMessage = new byte[] { 1, 2, 3, 4 };
+            string unserializedData = "some data";
+            var mockSerializer = new Mock<ISerializer>();
+            mockSerializer.Setup(m => m.Deserialize<NetworkMessage>(serializedMessage)).Returns(new NetworkMessage { Data = serializedData, Name = "System.String" });
+            mockSerializer.Setup(m => m.Deserialize<string>(serializedData)).Returns(unserializedData);
+
+            var mockConnection = new Mock<IConnection>();
+            var mockEventManager = new Mock<IEventManager>();
+
+            var objectConnection = new DefaultObjectConnection(mockConnection.Object) { Manager = mockEventManager.Object, Serializer = mockSerializer.Object };
+            bool OnCommandHasBeenCalled = false;
+            objectConnection.OnConnect((c) => OnCommandHasBeenCalled = true);
+
+            //Act
+            mockConnection.Raise(m => m.OnConnected += null);
+
+            Assert.IsTrue(OnCommandHasBeenCalled);
+        }
+
+        [Test]
+        public void Test_Lambda_OnDisconnect()
+        {
+            var testHandler = new TestHandler();
+
+            byte[] serializedData = new byte[] { 1, 2, 3 };
+            byte[] serializedMessage = new byte[] { 1, 2, 3, 4 };
+            string unserializedData = "some data";
+            var mockSerializer = new Mock<ISerializer>();
+            mockSerializer.Setup(m => m.Deserialize<NetworkMessage>(serializedMessage)).Returns(new NetworkMessage { Data = serializedData, Name = "System.String" });
+            mockSerializer.Setup(m => m.Deserialize<string>(serializedData)).Returns(unserializedData);
+
+            var mockConnection = new Mock<IConnection>();
+            var mockEventManager = new Mock<IEventManager>();
+
+            var objectConnection = new DefaultObjectConnection(mockConnection.Object) { Manager = mockEventManager.Object, Serializer = mockSerializer.Object };
+            bool OnCommandHasBeenCalled = false;
+            objectConnection.OnDisconnect((c) => OnCommandHasBeenCalled = true);
+
+            //Act
+            mockConnection.Raise(m => m.OnDisconnected += null);
+
+            Assert.IsTrue(OnCommandHasBeenCalled);
+        }
+
+        [Test]
+        public void Test_Lambda_RemoveOnCommand()
+        {
+            var testHandler = new TestHandler();
+
+            byte[] serializedData = new byte[] { 1, 2, 3 };
+            byte[] serializedMessage = new byte[] { 1, 2, 3, 4 };
+            string unserializedData = "some data";
+            var mockSerializer = new Mock<ISerializer>();
+            mockSerializer.Setup(m => m.Deserialize<NetworkMessage>(serializedMessage)).Returns(new NetworkMessage { Data = serializedData, Name = "System.String" });
+            mockSerializer.Setup(m => m.Deserialize<string>(serializedData)).Returns(unserializedData);
+
+            var mockConnection = new Mock<IConnection>();
+            var mockEventManager = new Mock<IEventManager>();
+
+            var objectConnection = new DefaultObjectConnection(mockConnection.Object) { Manager = mockEventManager.Object, Serializer = mockSerializer.Object };
+            bool OnCommandHasBeenCalled = false;
+            Action<IObjectConnection, string> del = (c, s) => OnCommandHasBeenCalled = true;
+            objectConnection.OnCommand<string>(del);
+            objectConnection.RemoveOnCommand<string>(del);
+
+            //Act
+            mockConnection.Raise(m => m.OnDataReceived += null, serializedMessage);
+
+            Assert.IsFalse(OnCommandHasBeenCalled);
+        }
+
+        [Test]
+        public void Test_Lambda_RemoveOnConnect()
+        {
+            var testHandler = new TestHandler();
+
+            byte[] serializedData = new byte[] { 1, 2, 3 };
+            byte[] serializedMessage = new byte[] { 1, 2, 3, 4 };
+            string unserializedData = "some data";
+            var mockSerializer = new Mock<ISerializer>();
+            mockSerializer.Setup(m => m.Deserialize<NetworkMessage>(serializedMessage)).Returns(new NetworkMessage { Data = serializedData, Name = "System.String" });
+            mockSerializer.Setup(m => m.Deserialize<string>(serializedData)).Returns(unserializedData);
+
+            var mockConnection = new Mock<IConnection>();
+            var mockEventManager = new Mock<IEventManager>();
+
+            var objectConnection = new DefaultObjectConnection(mockConnection.Object) { Manager = mockEventManager.Object, Serializer = mockSerializer.Object };
+            bool OnCommandHasBeenCalled = false;
+            Action<IObjectConnection> del = (c) => OnCommandHasBeenCalled = true;
+            objectConnection.OnConnect(del);
+            objectConnection.RemoveOnConnect(del);
+
+            //Act
+            mockConnection.Raise(m => m.OnConnected += null);
+
+            Assert.IsFalse(OnCommandHasBeenCalled);
+        }
+
+        [Test]
+        public void Test_Lambda_RemoveOnDisconnect()
+        {
+            var testHandler = new TestHandler();
+
+            byte[] serializedData = new byte[] { 1, 2, 3 };
+            byte[] serializedMessage = new byte[] { 1, 2, 3, 4 };
+            string unserializedData = "some data";
+            var mockSerializer = new Mock<ISerializer>();
+            mockSerializer.Setup(m => m.Deserialize<NetworkMessage>(serializedMessage)).Returns(new NetworkMessage { Data = serializedData, Name = "System.String" });
+            mockSerializer.Setup(m => m.Deserialize<string>(serializedData)).Returns(unserializedData);
+
+            var mockConnection = new Mock<IConnection>();
+            var mockEventManager = new Mock<IEventManager>();
+
+            var objectConnection = new DefaultObjectConnection(mockConnection.Object) { Manager = mockEventManager.Object, Serializer = mockSerializer.Object };
+            bool OnCommandHasBeenCalled = false;
+            Action<IObjectConnection> del = (c) => OnCommandHasBeenCalled = true;
+            objectConnection.OnDisconnect(del);
+            objectConnection.RemoveOnDisconnect(del);
+
+            //Act
+            mockConnection.Raise(m => m.OnDisconnected += null);
+
+            Assert.IsFalse(OnCommandHasBeenCalled);
+        }
+
+        [Test]
+        public void Test_Lambda_RemoveOnCommand_CustomCreator()
+        {
+            var testHandler = new TestHandler();
+
+            byte[] serializedData = new byte[] { 1, 2, 3 };
+            byte[] serializedMessage = new byte[] { 1, 2, 3, 4 };
+            string unserializedData = "some data";
+            var mockSerializer = new Mock<ISerializer>();
+            mockSerializer.Setup(m => m.Deserialize<NetworkMessage>(serializedMessage)).Returns(new NetworkMessage { Data = serializedData, Name = "System.String" });
+            mockSerializer.Setup(m => m.Deserialize<string>(serializedData)).Returns(unserializedData);
+
+            var mockConnection = new Mock<IConnection>();
+            var mockEventManager = new Mock<IEventManager>();
+
+            var objectConnection = new DefaultObjectConnection(mockConnection.Object) { Manager = mockEventManager.Object, Serializer = mockSerializer.Object };
+            bool OnCommandHasBeenCalled = false;
+            Action<IObjectConnection, string> del = (c, s) => OnCommandHasBeenCalled = true;
+            string creator = "CreatorObject :)";
+            objectConnection.OnCommand<string>(del, creator);
+            objectConnection.RemoveOnCommand<string>(creator);
+
+            //Act
+            mockConnection.Raise(m => m.OnDataReceived += null, serializedMessage);
+
+            Assert.IsFalse(OnCommandHasBeenCalled);
+        }
+
+        [Test]
+        public void Test_Lambda_RemoveOnCommand_CustomCreator_NonGeneric()
+        {
+            var testHandler = new TestHandler();
+
+            byte[] serializedData = new byte[] { 1, 2, 3 };
+            byte[] serializedMessage = new byte[] { 1, 2, 3, 4 };
+            string unserializedData = "some data";
+            var mockSerializer = new Mock<ISerializer>();
+            mockSerializer.Setup(m => m.Deserialize<NetworkMessage>(serializedMessage)).Returns(new NetworkMessage { Data = serializedData, Name = "System.String" });
+            mockSerializer.Setup(m => m.Deserialize<string>(serializedData)).Returns(unserializedData);
+
+            var mockConnection = new Mock<IConnection>();
+            var mockEventManager = new Mock<IEventManager>();
+
+            var objectConnection = new DefaultObjectConnection(mockConnection.Object) { Manager = mockEventManager.Object, Serializer = mockSerializer.Object };
+            bool OnCommandHasBeenCalled = false;
+            Action<IObjectConnection, string> del = (c, s) => OnCommandHasBeenCalled = true;
+            string creator = "CreatorObject :)";
+            objectConnection.OnCommand<string>(del, creator);
+            objectConnection.RemoveOnCommand(creator);
+
+            //Act
+            mockConnection.Raise(m => m.OnDataReceived += null, serializedMessage);
+
+            Assert.IsFalse(OnCommandHasBeenCalled);
+        }
+
+        [Test]
+        public void Test_Lambda_RemoveOnConnect_CustomCreator()
+        {
+            var testHandler = new TestHandler();
+
+            byte[] serializedData = new byte[] { 1, 2, 3 };
+            byte[] serializedMessage = new byte[] { 1, 2, 3, 4 };
+            string unserializedData = "some data";
+            var mockSerializer = new Mock<ISerializer>();
+            mockSerializer.Setup(m => m.Deserialize<NetworkMessage>(serializedMessage)).Returns(new NetworkMessage { Data = serializedData, Name = "System.String" });
+            mockSerializer.Setup(m => m.Deserialize<string>(serializedData)).Returns(unserializedData);
+
+            var mockConnection = new Mock<IConnection>();
+            var mockEventManager = new Mock<IEventManager>();
+
+            var objectConnection = new DefaultObjectConnection(mockConnection.Object) { Manager = mockEventManager.Object, Serializer = mockSerializer.Object };
+            bool OnCommandHasBeenCalled = false;
+            Action<IObjectConnection> del = (c) => OnCommandHasBeenCalled = true;
+            string creator = "CreatorObject :)";
+            objectConnection.OnConnect(del,creator);
+            objectConnection.RemoveOnConnect(creator);
+
+            //Act
+            mockConnection.Raise(m => m.OnConnected += null);
+
+            Assert.IsFalse(OnCommandHasBeenCalled);
+        }
+
+        [Test]
+        public void Test_Lambda_RemoveOnDisconnect_CustomCreator()
+        {
+            var testHandler = new TestHandler();
+
+            byte[] serializedData = new byte[] { 1, 2, 3 };
+            byte[] serializedMessage = new byte[] { 1, 2, 3, 4 };
+            string unserializedData = "some data";
+            var mockSerializer = new Mock<ISerializer>();
+            mockSerializer.Setup(m => m.Deserialize<NetworkMessage>(serializedMessage)).Returns(new NetworkMessage { Data = serializedData, Name = "System.String" });
+            mockSerializer.Setup(m => m.Deserialize<string>(serializedData)).Returns(unserializedData);
+
+            var mockConnection = new Mock<IConnection>();
+            var mockEventManager = new Mock<IEventManager>();
+
+            var objectConnection = new DefaultObjectConnection(mockConnection.Object) { Manager = mockEventManager.Object, Serializer = mockSerializer.Object };
+            bool OnCommandHasBeenCalled = false;
+            Action<IObjectConnection> del = (c) => OnCommandHasBeenCalled = true;
+            string creator = "CreatorObject :)";
+            objectConnection.OnDisconnect(del, creator);
+            objectConnection.RemoveOnDisconnect(creator);
+
+            //Act
+            mockConnection.Raise(m => m.OnDisconnected += null);
+
+            Assert.IsFalse(OnCommandHasBeenCalled);
+        }
+
         private class TestHandler
         {
             public bool HasOnConnectBeenCalled { get; private set; }
